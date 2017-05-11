5f7889f24453362132915456b8868c04ab148886
boissierflorian <boissierflorian@outlook.com>
2017-05-11 10:11:19 +0200

    Issue #4 Logs

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <glog/logging.h>
 
 using namespace std;
 
 int main()
 {
+  google::InitGoogleLogging("log_chifoumi");
+  google::SetLogDestination(google::GLOG_INFO, "log_chifoumi");
+  LOG(INFO) << "Init log..";
   cout << "Hello World !" << endl;
   return 0;
 }
